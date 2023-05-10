b541eb8c65b3a762e1830dcf083e4c1918af5ff3
lautumn1990 <932034966@qq.com>
2023-05-10 17:48:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+# wgcf-socks-docker
+
+a docker compose file for wgcf-socks
+
+## Usage
+
+```bash
+# use root
+git clone https://github.com/lautumn1990/wgcf-socks-docker
+cd wgcf-socks-docker
+
+docker-compose up -d
+
+# check and test
+curl --proxy socks5h://127.0.0.1:1080 https://www.cloudflare.com/cdn-cgi/trace
+```
+
+custom license and endpoint
+
+```bash
+# if you have warp license key
+mkdir wgcf
+echo "your_key" > wgcf/custom-wgcf-license.conf
+
+# like 
+# echo "3KOY1u56-pf2S09g7-xxxxxxx" > wgcf/custom-wgcf-license.conf
+
+
+# if you have a custom endpoint
+echo "your_endpoint" > wgcf/custom-wgcf-endpoint.conf
+
+# like
+# echo "162.159.195.168:5279" > wgcf/custom-wgcf-endpoint.conf
+
+```
+
+## reference
+
+- [Neilpang/wgcf-docker](https://github.com/Neilpang/wgcf-docker)
+- [Mon-ius/Docker-Warp-Socks](https://github.com/Mon-ius/Docker-Warp-Socks)
+- [vimagick/dante](https://hub.docker.com/r/vimagick/dante/)
---
+++ entry.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+_downwgcf() {
+  echo
+  echo "clean up"
+  if ! wg-quick down wgcf; then
+    echo "error down"
+  fi
+  echo "clean up done"
+  exit 0
+}
+
+
+
+#-4|-6
+runwgcf() {
+  trap '_downwgcf' ERR TERM INT
+
+  _enableV4="1"
+  if [ "$1" = "-6" ]; then
+    _enableV4=""
+  fi
+
+
+  if [ ! -e "wgcf-account.toml" ]; then
+    wgcf register --accept-tos
+  fi
+
+  if [ -e "custom-wgcf-license.conf" ]; then
+    _license_key=`cat custom-wgcf-license.conf`
+    sed -i "s/license_key.*$/license_key = '${_license_key}'/" wgcf-account.toml
+    wgcf update
+  fi
+
+  if [ ! -e "wgcf-profile.conf" ]; then
+    wgcf generate
+  fi
+
+  if [ -e "custom-wgcf-endpoint.conf" ]; then
+    _endpoint=`cat custom-wgcf-endpoint.conf`
+    sed -i "s/Endpoint.*$/Endpoint = ${_endpoint}/" wgcf-profile.conf
+  fi
+  
+  cp wgcf-profile.conf /etc/wireguard/wgcf.conf
+
+  DEFAULT_GATEWAY_NETWORK_CARD_NAME=`route  | grep default  | awk '{print $8}' | head -1`
+  DEFAULT_ROUTE_IP=`ifconfig $DEFAULT_GATEWAY_NETWORK_CARD_NAME | grep "inet " | awk '{print $2}' | sed "s/addr://"`
+  
+  echo ${DEFAULT_GATEWAY_NETWORK_CARD_NAME}
+  echo ${DEFAULT_ROUTE_IP}
+  
+  sed -i "/\[Interface\]/a PostDown = ip rule delete from $DEFAULT_ROUTE_IP  lookup main" /etc/wireguard/wgcf.conf
+  sed -i "/\[Interface\]/a PostUp = ip rule add from $DEFAULT_ROUTE_IP lookup main" /etc/wireguard/wgcf.conf
+
+  if [ "$1" = "-6" ]; then
+    sed -i 's/AllowedIPs = 0.0.0.0/#AllowedIPs = 0.0.0.0/' /etc/wireguard/wgcf.conf
+  elif [ "$1" = "-4" ]; then
+    sed -i 's/AllowedIPs = ::/#AllowedIPs = ::/' /etc/wireguard/wgcf.conf
+  fi
+
+
+  modprobe ip6table_raw
+  
+  wg-quick up wgcf
+  
+  if [ "$_enableV4" ]; then
+    _checkV4
+  else
+    _checkV6
+  fi
+
+  echo 
+  echo "OK, wgcf is up."
+  
+
+  sleep infinity & wait
+  
+  
+}
+
+_checkV4() {
+  echo "Checking network status, please wait...."
+  while ! curl --max-time 2  ipinfo.io; do
+    wg-quick down wgcf
+    echo "Sleep 2 and retry again."
+    sleep 2
+    wg-quick up wgcf
+  done
+
+
+}
+
+_checkV6() {
+  echo "Checking network status, please wait...."
+  while ! curl --max-time 2 -6 ipv6.google.com; do
+    wg-quick down wgcf
+    echo "Sleep 2 and retry again."
+    sleep 2
+    wg-quick up wgcf
+  done
+
+
+}
+
+
+
+if [ -z "$@" ] || [[ "$1" = -* ]]; then
+  runwgcf "$@"
+else
+  exec "$@"
+fi
+
+
